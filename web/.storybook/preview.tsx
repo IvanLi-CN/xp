306ed4fcf7f4a453034b3448134306e8102d8f1d
Ivan Li <ivanli2048@gmail.com>
2025-12-22 15:42:13 +0800

    fix(web): make storybook preview render
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/.storybook/preview.tsx
+++ web/.storybook/preview.tsx
@@ -1,23 +1,80 @@
 import type { Preview } from "@storybook/react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "@tanstack/react-router";
+import {
+	Outlet,
+	RouterProvider,
+	createMemoryHistory,
+	createRootRoute,
+	createRoute,
+	createRouter,
+} from "@tanstack/react-router";
 import React from "react";
 
 import "../src/styles.css";
 import { createQueryClient } from "../src/queryClient";
-import { createAppRouter } from "../src/router";
 
 const preview: Preview = {
 	decorators: [
 		(Story) => {
 			const queryClient = createQueryClient();
-			const router = createAppRouter();
+			const history = createMemoryHistory({
+				initialEntries: ["/"],
+			});
+
+			const rootRoute = createRootRoute({
+				component: () => <Outlet />,
+			});
+
+			const storyRoute = createRoute({
+				getParentRoute: () => rootRoute,
+				path: "/",
+				component: () => <Story />,
+			});
+
+			const nodesRoute = createRoute({
+				getParentRoute: () => rootRoute,
+				path: "/nodes",
+				component: () => null,
+			});
+
+			const endpointsRoute = createRoute({
+				getParentRoute: () => rootRoute,
+				path: "/endpoints",
+				component: () => null,
+			});
+
+			const usersRoute = createRoute({
+				getParentRoute: () => rootRoute,
+				path: "/users",
+				component: () => null,
+			});
+
+			const grantsRoute = createRoute({
+				getParentRoute: () => rootRoute,
+				path: "/grants",
+				component: () => null,
+			});
+
+			const loginRoute = createRoute({
+				getParentRoute: () => rootRoute,
+				path: "/login",
+				component: () => null,
+			});
+
+			const routeTree = rootRoute.addChildren([
+				storyRoute,
+				nodesRoute,
+				endpointsRoute,
+				usersRoute,
+				grantsRoute,
+				loginRoute,
+			]);
+
+			const router = createRouter({ routeTree, history });
 
 			return (
 				<QueryClientProvider client={queryClient}>
-					<RouterProvider router={router}>
-						<Story />
-					</RouterProvider>
+					<RouterProvider router={router} />
 				</QueryClientProvider>
 			);
 		},
