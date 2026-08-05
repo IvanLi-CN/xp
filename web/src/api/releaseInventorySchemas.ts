26c2ff40869e36770d6ca2f8c6456b164a7aa0a2
Ivan Li <ivanli2048@gmail.com>
2026-08-06 03:50:31 +0800

    chore(web): remove synthetic schema helper
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/src/api/releaseInventorySchemas.ts
+++
@@ -1,6 +0,0 @@
-export function completeSchemas(
-	routes: readonly string[],
-	known: Readonly<Record<string, readonly string[]>>,
-): Readonly<Record<string, readonly string[]>> {
-	return Object.fromEntries(routes.map((route) => [route, known[route] ?? []]));
-}
